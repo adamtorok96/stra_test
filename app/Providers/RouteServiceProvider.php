3b3fcc7cd0b960bc2d82d38a3118bcb0d3639a8b
edems <adamtorok@sch.bme.hu>
2019-05-20 17:20:33 +0200

    RouteServiceProvider upgraded

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -53,7 +53,15 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
              ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+             ->group(base_path('routes/web.php'))
+        ;
+
+        Route::middleware(['web', 'auth'])
+            ->namespace($this->namespace .'\Admin')
+            ->prefix('admin')
+            ->as('admin.')
+            ->group(base_path('routes/admin.php'))
+        ;
     }
 
     /**
