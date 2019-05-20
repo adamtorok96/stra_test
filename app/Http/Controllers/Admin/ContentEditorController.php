cbacd4f3fcf7d5ead12c17ce130aa25efefda095
edems <adamtorok@sch.bme.hu>
2019-05-20 17:18:38 +0200

    Content Editor controller & view

---
+++ app/Http/Controllers/Admin/ContentEditorController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin;
+
+
+class ContentEditorController
+{
+    public function index()
+    {
+        return view('admin.content-editor.index');
+    }
+}
---
+++ resources/views/admin/content-editor/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+
+@section('content')
+    <h1>Tartalomszerkesztő</h1>
+@endsection
