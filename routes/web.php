2fc28e8d11da758b5f28084d3839e8101a53e86a
edems <adamtorok@sch.bme.hu>
2019-05-20 17:19:41 +0200

    routes added

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,10 @@
 <?php
+
+Route::get('/', 'HomeController@index')->name('home.index');
+
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function ()
+{
+    Route::get('/', 'AuthController@index')->name('index');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::get('logout', 'AuthController@logout')->name('logout');
+});
