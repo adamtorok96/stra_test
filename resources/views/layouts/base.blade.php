672af0289ebcfb614e3f65de2f5c82e991cf603f
edems <adamtorok@sch.bme.hu>
2019-05-20 17:19:55 +0200

    base layout added

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <title>Stra.test</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
